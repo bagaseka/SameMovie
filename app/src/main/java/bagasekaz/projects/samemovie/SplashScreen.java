e626b87a0e3f45fb4f9754c74f1a7ec235af0aa4
SameHiu <bagaseka4@gmail.com>
2023-08-30 21:02:17 +0700

    New Project

---
+++ app/src/main/java/bagasekaz/projects/samemovie/SplashScreen.java
@@ -0,0 +1,19 @@
+package bagasekaz.projects.samemovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+    }
+}
