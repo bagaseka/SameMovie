d036f21d885d8044d2c02dfccc92a467fd2a0b0c
SameHiu <bagaseka4@gmail.com>
2023-08-31 09:42:57 +0700

    SplashScreen -Checking if local data is empty

---
+++ app/src/main/java/bagasekaz/projects/samemovie/helper/NetworkHelper.java
@@ -0,0 +1,21 @@
+package bagasekaz.projects.samemovie.helper;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+public class NetworkHelper {
+
+    public static boolean isInternetConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        }
+
+        return false;
+    }
+
+}
